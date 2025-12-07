4e8a94bb2491feedd537c967d20d5fe5ae3f3e27
YAABITECH <yaabitech@gmail.com>
2025-12-07 14:27:42 +0530

    removed unwanted public files

--- public/test-series/dyntimecount.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$startTime = $_SESSION['t_starttime'];
-$duration = $_SESSION['t_duration'];
-$timediff = ($duration*60)-(time()-$startTime);
-echo $timediff;
-//echo (floor($timediff/60)).":".(floor($timediff%60));
-?>
